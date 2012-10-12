8ca50e201019790a9f128de2f4425dcdaeb41503
Rick Gibbed <gibbed@users.noreply.github.com>
2012-10-12 19:05:40 +0000

    - Added ReadStringArray in Engine. - Added support for StrField in ArrayField. - Added support for static arrays in NameField, ObjectField. - Added proper support for ClassField.

--- Unreflect.Core/UnrealFields/ClassField.cs
+++ Unreflect.Core/UnrealFields/ClassField.cs
@@ -7,5 +7,20 @@ namespace Unreflect.Core.UnrealFields
 {
     internal class ClassField : UnrealField
     {
+        internal override object Read(Engine engine, IntPtr objectAddress)
+        {
+            var classAddress = engine.ReadPointer(objectAddress + this.Offset);
+            if (classAddress == IntPtr.Zero)
+            {
+                return (UnrealObject)null;
+            }
+
+            var uclass = engine.GetClass(classAddress);
+            if (uclass == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return uclass;
+        }
     }
 }
